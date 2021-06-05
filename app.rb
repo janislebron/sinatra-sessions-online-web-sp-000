92c1ac88a44f40cd60a62ee376c13d239fa2a7ee
Janis Lebron <janislebron@gmail.com>
2021-06-05 16:45:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,6 +19,8 @@ class App < Sinatra::Base
   end
 
   get '/set' do
+    session[:foo] = 'hello'
+    @session = session
     # set the :foo key of the session hash equal to 'hello' here!
     if session[:foo] == 'hello'
       redirect '/fetch'
